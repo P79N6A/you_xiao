2441d942ac8b5bedae09330b757584f0bc57b17d
qiaojunchao <qiaojunchao@runtoinfo.com>
2018-08-31 09:26:51 +0800

    新增评论点赞回复模块 Version：1.1.5

---
+++ comment-praise-reply/src/main/java/com/youxiao/comment/activity/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.youxiao.comment.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by QiaoJunChao on 2018/8/31.
+ */
+
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ comment-praise-reply/src/main/java/com/youxiao/comment/activity/PublishComment.java
@@ -0,0 +1,16 @@
+package com.youxiao.comment.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.youxiao.comment.R;
+
+public class PublishComment extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish_comment);
+    }
+}
